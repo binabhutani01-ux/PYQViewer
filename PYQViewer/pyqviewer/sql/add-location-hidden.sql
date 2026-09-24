32d4391445f2d60740e2976a2ab119e352e8bbc3
binabhutani01-ux <binabhutani01@gmail.com>
2026-09-24 10:12:17 +0530

    Delete PYQViewer/pyqviewer directory

--- PYQViewer/pyqviewer/sql/add-location-hidden.sql
+++
@@ -1,5 +0,0 @@
--- PYQViewer: hide map pins without deleting login/audit records
-alter table login_events
-add column if not exists location_hidden boolean not null default false;
-
-NOTIFY pgrst, 'reload schema';
